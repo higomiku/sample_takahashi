29d67c9f47075610b32cc0a04549968305b1a642
higomiku2012 <higomiku2012@gmail.com>
2020-10-24 21:45:53 +0900

    audio_recorderの断面で保存

--- sample_takahashi/takahashi_alarm/lib/main.dart
+++ sample_takahashi/takahashi_alarm/lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:io' as io;
+import 'dart:math';
+
+import 'package:ext_storage/ext_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 import 'next_page.dart';
 
@@ -6,8 +11,15 @@ import 'package:assets_audio_player/assets_audio_player.dart';
 
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
+//AudioRecorderのimport宣言
+import 'package:audio_recorder/audio_recorder.dart';
+import 'package:file/file.dart';
+import 'package:file/local.dart';
+import 'package:path_provider/path_provider.dart';
+
 void main() {
-  runApp(MyApp());
+  //runApp(MyApp());
+  runApp(MyAudioRecordApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -53,6 +65,122 @@ class MyHomePage extends StatefulWidget {
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
+}
+
+class MyAudioRecordApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyAudioRecordApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Plugin audio recorder'),
+        ),
+        body: new AppBody(),
+      ),
+    );
+  }
+}
+
+class AppBody extends StatefulWidget {
+  final LocalFileSystem localFileSystem;
+
+  AppBody({localFileSystem})
+      : this.localFileSystem = localFileSystem ?? LocalFileSystem();
+
+  @override
+  State<StatefulWidget> createState() => new AppBodyState();
+}
+
+class AppBodyState extends State<AppBody> {
+  Recording _recording = new Recording();
+  bool _isRecording = false;
+  Random random = new Random();
+  TextEditingController _controller = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return new Center(
+      child: new Padding(
+        padding: new EdgeInsets.all(8.0),
+        child: new Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              new FlatButton(
+                onPressed: _isRecording ? null : _start,
+                child: new Text("Start"),
+                color: Colors.green,
+              ),
+              new FlatButton(
+                onPressed: _isRecording ? _stop : null,
+                child: new Text("Stop"),
+                color: Colors.red,
+              ),
+              new TextField(
+                controller: _controller,
+                decoration: new InputDecoration(
+                  hintText: 'Enter a custom path',
+                ),
+              ),
+              new Text("File path of the record: ${_recording.path}"),
+              new Text("Format: ${_recording.audioOutputFormat}"),
+              new Text("Extension : ${_recording.extension}"),
+              new Text(
+                  "Audio recording duration : ${_recording.duration.toString()}")
+            ]),
+      ),
+    );
+  }
+
+  _start() async {
+    try {
+      var path = await ExtStorage.getExternalStoragePublicDirectory(
+          ExtStorage.DIRECTORY_MUSIC);
+      print("path=" + path);
+      if (await AudioRecorder.hasPermissions) {
+        if (_controller.text != null && _controller.text != "") {
+          String path = _controller.text;
+          if (!_controller.text.contains('/')) {
+            io.Directory appDocDirectory =
+                await getApplicationDocumentsDirectory();
+            path = appDocDirectory.path + '/' + _controller.text;
+          }
+          print("Start recording: $path");
+          await AudioRecorder.start(
+              path: path, audioOutputFormat: AudioOutputFormat.AAC);
+        } else {
+          await AudioRecorder.start();
+        }
+        bool isRecording = await AudioRecorder.isRecording;
+        setState(() {
+          _recording = new Recording(duration: new Duration(), path: "");
+          _isRecording = isRecording;
+        });
+      } else {
+        Scaffold.of(context).showSnackBar(
+            new SnackBar(content: new Text("You must accept permissions")));
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  _stop() async {
+    var recording = await AudioRecorder.stop();
+    print("Stop recording: ${recording.path}");
+    bool isRecording = await AudioRecorder.isRecording;
+    File file = widget.localFileSystem.file(recording.path);
+    print("  File length: ${await file.length()}");
+    setState(() {
+      _recording = recording;
+      _isRecording = isRecording;
+    });
+    _controller.text = recording.path;
+  }
 }
 
 class NotificationContent {
